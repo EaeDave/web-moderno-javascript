1bccf227ce0e7c0a79c419b92e890387557f3f2d
EaeDave <eaedav3@gmail.com>
2025-01-25 10:30:29 -0300

    Comentários de Código

---
+++ fundamentos/inicio/comentario.js
@@ -0,0 +1,12 @@
+// console.log é usado para exibir algo no terminal/console/output!
+console.log("Passo #03");  // o código é interpretado da esquerda pra direita
+console.log("Passo #01");  // de cima para baixo.
+console.log("Passo #02");
+
+/**
+ * Isso é um
+ * comentário
+ * de
+ * multiplas
+ * linhas
+ */
