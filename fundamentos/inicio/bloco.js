0efc9ed5928fcd4edf6ddd0c724d85789545cf64
EaeDave <EaeDav3@gmail.com>
2025-01-26 08:18:10 -0300

    Bloco de Código

---
+++ fundamentos/inicio/bloco.js
@@ -0,0 +1,20 @@
+// Isso é um bloco de código
+{
+    console.log("Passo #01");
+    console.log("Passo #02");
+    console.log("Passo #03");
+}
+
+
+// Isso é um outro bloco de código
+{
+    console.log("Passo #04");
+    console.log("Passo #05");
+}
+
+
+{
+    {
+        console.log("Um bloco de código dentro de outro.")
+    }
+}
