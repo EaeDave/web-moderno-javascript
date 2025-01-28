80774accc56273a9dce57e87c88b056e40c71661
EaeDave <EaeDav3@gmail.com>
2025-01-28 20:00:35 -0300

    Trabalhando com Dados #01

---
+++ fundamentos/inicio/dados_1.js
@@ -0,0 +1,14 @@
+var nome = "Caneta";  // declarando variável usando var
+var quantidade = 10;
+var preco = 6.4;
+let imposto = 1.5;
+let precoFinal = preco + imposto;  // declarando variável usando let
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Caneta BIC";  // a variável "nome" foi modificada e agora representa outro valor
+console.log(nome);
