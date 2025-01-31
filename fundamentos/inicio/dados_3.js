5b6848a150b6aa8cb000e643b5f80595a2b1efac
EaeDave <eaedav3@gmail.com>
2025-01-31 10:17:55 -0300

    Trabalhando com Dados #03

---
+++ fundamentos/inicio/dados_3.js
@@ -0,0 +1,13 @@
+// Alguns tipos de dados
+
+let idade = 23;  // number
+console.log(typeof(idade));
+
+let salario = 2168.0;  // number
+console.log(typeof(salario))
+
+let estaChovendo = false;
+console.log(typeof(estaChovendo));  // boolean
+
+let nome = "David";
+console.log(typeof(nome));  // string
