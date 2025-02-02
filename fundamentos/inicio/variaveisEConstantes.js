ad04056be85e68dbf92dc498b99710bef3875d44
EaeDave <EaeDav3@gmail.com>
2025-02-02 08:30:30 -0300

    O Básico de Var, Let e Const

---
+++ fundamentos/inicio/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4  // Forma mais moderna de declarar variáveis
+
+var a = 30  // Usando let é possível redeclarar uma variável
+// let b = 40  enquanto com let não
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+// c = 50
+console.log(c)
