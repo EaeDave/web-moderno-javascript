c28565c080e626309b5897d30738c440a8705107
EaeDave <EaeDav3@gmail.com>
2025-02-02 08:51:06 -0300

    Tipagem fraca

---
+++ fundamentos/inicio/tipagemFraca.js
@@ -0,0 +1,14 @@
+// O Javascript é uma linguagem de tipagem fraca ou dinâmica
+// Ao declarar uma variável, intuitivamente ele já identifica o tipo do dado
+let qualquer = "legal"  // É identificado como string
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.1415  // Tipo number
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nomes genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false // Produto Químico Perigoso... kkk
