3a4b748d2bc55ce02bb84f698b56ff3533bad5a7
EaeDave <EaeDav3@gmail.com>
2025-02-17 17:47:51 -0300

    Tipos em JavaScript: Object

---
+++ fundamentos/inicio/objeto.js
@@ -0,0 +1,12 @@
+const prod1 = {}  // Objeto
+prod1.nome = "Poco X6"  // Adicionando um atributo dinamicamente para um objeto vazio
+prod1.preco = 1699.99
+
+console.log(prod1)
+
+const prod2 = {  // Criando o objeto já com os atributos feitos
+    nome: "Iphone 8",
+    preco: 900.00,
+}
+
+console.log(prod2)
